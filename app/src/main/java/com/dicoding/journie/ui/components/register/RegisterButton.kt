98bdc94733dd4bbaab920a6faf413b4a42f6e33b
arkefallen <rafa9011@gmail.com>
2023-06-04 13:12:01 +0700

    add text input field

---
+++ app/src/main/java/com/dicoding/journie/ui/components/register/RegisterButton.kt
@@ -0,0 +1,4 @@
+package com.dicoding.journie.ui.components.register
+
+class RegisterButton {
+}
---
+++ app/src/main/java/com/dicoding/journie/ui/components/register/TextInput.kt
@@ -0,0 +1,11 @@
+package com.dicoding.journie.ui.components.register
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TextField(
+    label: String,
+    placeholder: String
+) {
+
+}
