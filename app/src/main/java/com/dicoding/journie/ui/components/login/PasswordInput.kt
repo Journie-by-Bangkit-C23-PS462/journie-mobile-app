a6a471bb56ca90b8874fec1399bd506578b076dd
arkefallen <rafa9011@gmail.com>
2023-06-04 19:30:23 +0700

    add google register button

--- app/src/main/java/com/dicoding/journie/ui/components/login/PasswordInput.kt
+++ app/src/main/java/com/dicoding/journie/ui/components/login/PasswordInput.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -13,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dicoding.journie.R
@@ -23,6 +25,7 @@ fun PasswordInput(
     modifier: Modifier = Modifier
 ) {
     var value by remember { mutableStateOf("") }
+    var passwordVisibility by remember { mutableStateOf(false) }
     OutlinedTextField(
         value = value,
         onValueChange = { newText ->
@@ -34,7 +37,7 @@ fun PasswordInput(
         modifier = modifier
             .fillMaxWidth()
             .heightIn(min = 48.dp),
-        visualTransformation = PasswordVisualTransformation()
+        visualTransformation = if ( passwordVisibility ) VisualTransformation.None else PasswordVisualTransformation(),
     )
 }
 
